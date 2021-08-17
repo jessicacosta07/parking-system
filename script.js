fb074d513d0dd52918dec7e01536e9167e67e0ac
jessicacosta07 <jessikacostajf@gmail.com>
2021-08-17 18:36:26 -0300

    parte adicionar

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 (function () {
     var _a;
     var $ = function (query) { return document.querySelector(query); };
+    function patio() {
+        function ler() {
+        }
+        function adicionar(veiculo) {
+            var _a;
+            var row = document.createElement("tr");
+            row.innerHTML = "\n            <td>" + veiculo.nome + "</td>\n            <td>" + veiculo.placa + "</td>\n            <td>" + veiculo.entrada + "</td>\n            <td>\n                <button class=\"delete\" data-placa=\"" + veiculo.placa + "\">X</button>\n            </td>\n            ";
+            (_a = $('#patio')) === null || _a === void 0 ? void 0 : _a.appendChild(row);
+        }
+        function remover() {
+        }
+        function salvar() {
+        }
+        function render() {
+        }
+        return { ler: ler, adicionar: adicionar, remover: remover, salvar: salvar, render: render };
+    }
     (_a = $('#cadastrar')) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function () {
         var _a, _b;
         var nome = (_a = $('#nome')) === null || _a === void 0 ? void 0 : _a.value;
@@ -9,5 +26,6 @@
             alert('Os campos noma e placa são obrigatórios');
             return;
         }
+        patio().adicionar({ nome: nome, placa: placa, entrada: new Date() });
     });
 })();
--- script.ts
+++ script.ts
@@ -1,5 +1,40 @@
+interface Veiculo {
+    nome: string,
+    placa: string,
+    entrada: Date
+}
+
 (function () {
     const $ = (query: string): HTMLInputElement | null => document.querySelector(query);
+
+    function patio() {
+
+        function ler() {
+
+        }
+        function adicionar(veiculo: Veiculo) {
+            const row = document.createElement("tr")
+            row.innerHTML = `
+            <td>${veiculo.nome}</td>
+            <td>${veiculo.placa}</td>
+            <td>${veiculo.entrada}</td>
+            <td>
+                <button class="delete" data-placa="${veiculo.placa}">X</button>
+            </td>
+            `;
+            $('#patio')?.appendChild(row);
+        }
+        function remover() {
+
+        }
+        function salvar() {
+
+        }
+        function render() {
+
+        }
+        return { ler, adicionar, remover, salvar, render }
+    }
 
     $('#cadastrar')?.addEventListener("click", () => {
         const nome = $('#nome')?.value;
@@ -9,5 +44,6 @@
             alert('Os campos noma e placa são obrigatórios')
             return;
         }
+        patio().adicionar({ nome, placa, entrada: new Date() })
     })
 })()
